21658c1ab7eabfee3cda254f49fa47d45cb361aa
Vlad-unitbv <vladcurticapean05@gmail.com>
2026-02-10 14:50:58 +0200

    configs

---
+++ src/main/java/com/sensorguard/gateway/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package com.sensorguard.gateway.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI sensorGuardOpenAPI() {
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+        localServer.setDescription("Local Development Server");
+
+        Contact contact = new Contact();
+        contact.setName("SensorGuard Team");
+        contact.setEmail("support@sensorguard.com");
+
+        License license = new License()
+                .name("MIT License")
+                .url("https://opensource.org/licenses/MIT");
+
+        Info info = new Info()
+                .title("SensorGuard API")
+                .version("1.0.0")
+                .description("Cloud-native sensor monitoring system with real-time anomaly detection")
+                .contact(contact)
+                .license(license);
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(localServer));
+    }
+}
